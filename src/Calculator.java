42d52e2821d1b1a58864bfdf7187c57bd4a97ff1
notagirl <mordsithapple@gmail.com>
2021-05-03 17:48:13 +0300

    "Additional changes"

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,62 @@
+import java.util.Scanner;
+
 public class Calculator {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.printf("Hello world!!!");
+        System.out.println("Введите что-нибудь");
+        String expression = scanner.nextLine();
+        String[] data = expression.replace(" ", "").split("(?=\\+)|(?<=\\+)|(?=\\*)|(?<=\\*)|(?=\\-)|(?<=\\-)|(?=\\\\)|(?<=\\\\)");
+        try {
+            String result = String.valueOf(processInput(data));
+            System.out.println(result);
+        } catch (Exception ex) {
+            System.out.println("Что-то пошло не так, проверьте ввод");
+        }
+
+
+    }
+
+    static String processInput(String[] data) throws Exception {
+        if (RomanNumberTranslator.translateFromRoman(data[0]) == null) {
+            int firstOperand = Integer.parseInt(data[0]);
+            int secondOperand = Integer.parseInt(data[2]);
+            if(firstOperand > 10 | secondOperand > 10){
+                throw new Exception();
+            }
+            char operation = data[1].charAt(0);
+            return calc(firstOperand, secondOperand, operation);
+        }
+        if (RomanNumberTranslator.translateFromRoman(data[0]) != null) {
+            int firstOperand = Integer.parseInt(RomanNumberTranslator.translateFromRoman(data[0]));
+            int secondOperand = Integer.parseInt(RomanNumberTranslator.translateFromRoman(data[2]));
+            char operation = data[1].charAt(0);
+            String result = calc(firstOperand, secondOperand, operation);
+            String romanResult = RomanNumberTranslator.translateToRoman(result);
+            return romanResult;
+        }
+        return null;
+
+    }
+
+    public static String calc(int num1, int num2, char operation) {
+        int result = 0;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                result = 0;
+        }
+        return String.valueOf(result);
     }
 }
---
+++ src/RomanNumberTranslator.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+
+public class RomanNumberTranslator {
+
+    public static String translateFromRoman(String num) {
+        switch (num) {
+            case "I":
+                return "1";
+            case "II":
+                return "2";
+            case "III":
+                return "3";
+            case "IV":
+                return "4";
+            case "V":
+                return "5";
+            case "VI":
+                return "6";
+            case "VII":
+                return "7";
+            case "VIII":
+                return "8";
+            case "IX":
+                return "9";
+            case "X":
+                return "10";
+        }
+        return null;
+    }
+
+    public static String translateToRoman(String value) {
+        int num = Integer.parseInt(value);
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanLiterals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder roman = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                roman.append(romanLiterals[i]);
+            }
+        }
+        return roman.toString();
+    }
+}
+
